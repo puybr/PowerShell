cf1d02166b2e15c55840054f68e9293ce54ede1b
chl03 <56723522+ghost-goblin@users.noreply.github.com>
2021-09-24 15:18:22 +0100

    Create addADGroup.ps1

---
+++ addADGroup.ps1
@@ -0,0 +1,18 @@
+# Move computer objects to OU
+Import-Module ActiveDirectory
+$computers = import-csv C:\scripts\computers-to-be-imaged.csv
+Foreach ($computer in $computers) {
+Get-ADComputer $computer.name | Move-ADObject -TargetPath "OU=Taget,DC=local" -Verbose
+}
+
+
+# Get all the computers in an OU
+Get-ADcomputer -Filter * -SearchBase "OU=Taget,DC=local" | Sort-Object -Property Name | ft Name
+
+
+## Script to add all computers in ou to an AD group
+$laptops = Get-ADComputer -Filter * -SearchBase "OU=Taget,DC=local" -SearchScope 2 | Where-Object {($_.Name -match "LAPTOP*") -or ($_.Name -match "COMPUTER*")}
+foreach ($laptop in $laptops) {
+    Remove-ADGroupMember "Group Name" -Members $laptop -Confirm:$false
+    Add-ADGroupMember "Group Name" -Members $laptop
+}
