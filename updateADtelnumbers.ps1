e0e9eaafdcffce7c817ace598cc40215f34fe629
chl03 <56723522+ghost-goblin@users.noreply.github.com>
2022-07-20 08:22:14 +0100

    Update updateADtelnumbers.ps1

--- updateADtelnumbers.ps1
+++ updateADtelnumbers.ps1
@@ -1,6 +1,6 @@
 Import-Module ActiveDirectory
 # # #
-$users = import-csv C:\scripts\userddphonestest.csv
+$users = import-csv C:\scripts\userphones.csv
 Foreach ($user in $users) {
     Get-Aduser -filter * -Properties * | ? {$_.UserPrincipalName -eq $user.EmailAddress } | Set-ADUser -OfficePhone $user.Tel
 } 
