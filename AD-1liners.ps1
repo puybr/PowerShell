379392ed0e7bcb1a20db87d6ebc80bbdfc490d54
chl03 <56723522+ghost-goblin@users.noreply.github.com>
2022-08-23 14:39:22 +0100

    Update AD-1liners.ps1

--- AD-1liners.ps1
+++ AD-1liners.ps1
@@ -1,5 +1,7 @@
 # # # USEFUL AD ONE LINERS
 
 Search-ADAccount -Lockedout | Unlock-AdAccount -Confirm
+
 Get-Aduser -filter * -Properties * | ? {$_.PasswordExpired -eq $True} | Ft Name
+
 Get-Aduser -filter * -Properties * | ? {$_.PasswordNeverExpires -eq $True} | Ft Name
