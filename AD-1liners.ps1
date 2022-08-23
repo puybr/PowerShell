26f77deced38882f0984864619afcff450a65da0
chl03 <56723522+ghost-goblin@users.noreply.github.com>
2022-08-23 12:54:13 +0100

    Update AD-1liners.ps1

--- AD-1liners.ps1
+++ AD-1liners.ps1
@@ -3,5 +3,5 @@ Import-Module ActiveDirectory
 # # # USEFUL AD ONE LINERS
 
 Search-ADAccount -Lockedout | Unlock-AdAccount -Confirm
-Get-Aduser -filter * -Properties * -SearchBase "OU=Standard,OU=Users,OU=Brighton,OU=Vero Screening,DC=veroscreening,DC=local" | ? {$_.PasswordExpired -eq $True} | Ft Name
-Get-Aduser -filter * -Properties * -SearchBase "OU=Standard,OU=Users,OU=Brighton,OU=Vero Screening,DC=veroscreening,DC=local" | ? {$_.PasswordNeverExpires -eq $True} | Ft Name
+Get-Aduser -filter * -Properties * | ? {$_.PasswordExpired -eq $True} | Ft Name
+Get-Aduser -filter * -Properties * | ? {$_.PasswordNeverExpires -eq $True} | Ft Name
