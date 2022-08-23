266ffc070f86952654e0cd2465a3aca5883d4c51
chl03 <56723522+ghost-goblin@users.noreply.github.com>
2022-08-23 13:09:58 +0100

    Update AD-movecomputers.ps1

--- AD-movecomputers.ps1
+++ AD-movecomputers.ps1
@@ -2,15 +2,12 @@
 Import-Module ActiveDirectory
 $computers = import-csv C:\scripts\computers.csv
 
-
-
 # Get all the computers in an OU
 Get-ADcomputer -Filter * -SearchBase "OU=Taget,DC=local" | Sort-Object -Property Name | ft Name
 
 Foreach ($computer in $computers) {
 Get-ADComputer $computer.name | Move-ADObject -TargetPath "OU=Taget,DC=local" -Verbose
 }
-
 
 ## Script to add all computers in ou to an AD group
 $laptops = Get-ADComputer -Filter * -SearchBase "OU=Taget,DC=local" -SearchScope 2 | Where-Object {($_.Name -match "LAPTOP*") -or ($_.Name -match "COMPUTER*")}
