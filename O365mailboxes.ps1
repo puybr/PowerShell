140714ba5e3bd46de81d482811c5c69efd6e2c2a
chl03 <56723522+ghost-goblin@users.noreply.github.com>
2023-02-07 11:24:21 +0000

    Update O365mailboxes.ps1

--- O365mailboxes.ps1
+++ O365mailboxes.ps1
@@ -1,1 +1,14 @@
 
+# Connect to Exchange Online
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -UserPrincipalName name@domain.com
+Import-Module MSOnline
+Connect-MsolService
+
+### IN HOUSE EXCHANGE ###
+$ExchSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri 'http://mailserver.domain.local/PowerShell/'
+Import-PSSession $ExchSession
+### New Mailbox
+New-RemoteMailbox -Name "Mailbox Name" -UserPrincipalName "mailbox@domain.com" -OnPremisesOrganizationalUnit "OU=MailoxOU,OU=Users,OU=Org,DC=domain,DC=local"
+
+Get-Mailbox -Public
