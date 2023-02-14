a3311636f4aefba2e9962c55a3b4351d20c5f3f7
chl03 <56723522+ghost-goblin@users.noreply.github.com>
2023-02-14 13:00:06 +0000

    Update certificates.ps1

--- certificates.ps1
+++ certificates.ps1
@@ -1,8 +1,8 @@
 $mypwd = ConvertTo-SecureString "p@ssword" -asplaintext -force
 $certpath = "\\path\to\share\certificate.pfx"
-$CIFAthumbprint = "####" 
+$thumbprint = "####" 
 
-$Thumbprint = (Get-ChildItem -Path Cert:\CurrentUser\My| Where-Object {$_.Thumbprint -match $CIFAthumbprint}).Thumbprint;
+$Thumbprint = (Get-ChildItem -Path Cert:\CurrentUser\My| Where-Object {$_.Thumbprint -match $thumbprint}).Thumbprint;
 ### Write-Host -Object "My thumbprint is: $Thumbprint"
 if ($Thumbprint -eq $null ) { Import-PfxCertificate -FilePath $certpath -CertStoreLocation Cert:\CurrentUser\My -Password $mypwd
 } else { exit } sleep 10
